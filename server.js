6e20bb63cd754cf434e3ecc48897461df2e3e405
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-30 21:23:00 -0800

    changing port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const port = 3000
+const port = process.env.PORT || 3001
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const { readAndAppend, writeToFile, readFromFile } = require('./helpers/fsUtils');
