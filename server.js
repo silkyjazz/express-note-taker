9c8a073caa3412a0f8b22f21424b9b671d7c9131
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-30 21:17:28 -0800

    getting api

--- server.js
+++ server.js
@@ -22,47 +22,33 @@ app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
 })
 
-app.post('/api/notes', (req, res) =>{ 
-    console.log(req.body)
-    // console.log(req)
-    const {title, text} = req.body
+// app.post('/api/notes', (req, res) =>{ 
+//     console.log(req.body)
+//    
+//     const {title, text} = req.body
 
-    console.log(req.body)
-    let notesArr = []
-    if(title && text){
-        const newNote = {
-            title,
-            text,
-            id: uuid()
-        }
+//     console.log(req.body)
+//     let notesArr = []
+//     if(title && text){
+//         const newNote = {
+//             title,
+//             text,
+//             id: uuid()
+//         }
 
-        readAndAppend(newNote, './db/db.json');
-        // fs.readFile('./db/db.json', 'utf-8', (err, data) =>{
-        //     if (err){
-        //         console.log(err)
-        //     }else{
-        //         //converting string into object
-        //         notesArr = JSON.parse(data)
-        //         //adding new note
-        //         notesArr.push(newNote)
-        //         fs.writeFile('./db/db.json', JSON.stringify(notesArr, null, 4),
-        //         (writeErr) => writeErr ? console.error(writeErr) : console.info('Succeessfully added new note')
-        //         )
+//         readAndAppend(newNote, './db/db.json');
 
-        //     }
-        // })
+//         const response = {
+//             status: 'success',
+//             body: newNote,
+//         };
+//         console.log(response)
+//         res.status(201).json(response)
+//     }else{
+//         res.status(500).json('Error adding new note')
+//     }
 
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-        console.log(response)
-        res.status(201).json(response)
-    }else{
-        res.status(500).json('Error adding new note')
-    }
-
-})
+// })
 
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname), '/public/index.html'))
